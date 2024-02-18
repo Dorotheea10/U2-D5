b202589f516c1ee33d2bb091edec5c3f4352bd36
Dorotheea10 <150785849+Dorotheea10@users.noreply.github.com>
2024-02-18 23:54:22 +0100

    Finito

--- base_di_partenza/assets/css/js/main.js
+++ base_di_partenza/assets/css/js/main.js
@@ -1,12 +1,9 @@
-// Aggiungi uno script JavaScript alla fine del tuo documento HTML
-document.addEventListener("scroll", function () {
-  var navbar = document.querySelector(".navbar");
-  var scrollPosition = window.scrollY;
-
-  // Aggiungi la classe "navbar-scrolled" quando la posizione di scorrimento supera la sezione gialla
-  if (scrollPosition > document.querySelector(".hero-section").offsetHeight) {
-    navbar.classList.add("navbar-scrolled");
+const EVENT_POSY = 400;
+const startedButtonRef = document.getElementById("startedButton");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= EVENT_POSY) {
+    startedButtonRef.classList.add("green-button");
   } else {
-    navbar.classList.remove("navbar-scrolled");
+    startedButtonRef.classList.remove("green-button");
   }
 });
